b4cc00992b9c48b75b04a3f65684796bb1e09ed6
Merce Pereira <merce.pereira.cao@gmail.com>
2016-11-23 21:43:40 +0100

    second sample modified to ask two numbers to enter

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 // app/index.js
 const calc = require('./calc')
+const readline = require('readline');
 
 const numbersToAdd = [
     3,
@@ -14,3 +15,24 @@ const result = calc.sum(numbersToAdd)
 // hey were called "template strings" in prior editions
 // Template literals are enclosed by the back-tick (` `)
 console.log(`the result is ${result}`)
+
+//write a small application that asks the user for two numbers to enter 
+// from the standard input and print the result after it. 
+// (hint: use the readline module: https://nodejs.org/api/readline.html)
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Give me 2 numbers ', (answer) => {
+  // TODO: Log the answer in a database
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+  rl.close();
+});
+
+
+
+
+
+
+
