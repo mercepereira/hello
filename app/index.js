e90865ecf09e874ce9079bd49db94a957723c1a4
Merce Pereira <merce.pereira.cao@gmail.com>
2016-11-23 20:47:30 +0100

    second sample modified to include template literal

--- app/index.js
+++ app/index.js
@@ -9,4 +9,8 @@ const numbersToAdd = [
 ]
 
 const result = calc.sum(numbersToAdd)
-console.log('the result is ' + result)
+//console.log('the result is ' + result)
+// Template literals are string literals allowing embedded expressions. 
+// hey were called "template strings" in prior editions
+// Template literals are enclosed by the back-tick (` `)
+console.log(`the result is ${result}`)
